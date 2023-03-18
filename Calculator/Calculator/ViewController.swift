0591797765c80acb96c7124573f1014ae3c90975
mohamdan <engineerm86@gmail.com>
2023-03-18 21:04:27 +0200

    UI-linked-VC

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayLbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calcBtnPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func numBtnPressed(_ sender: UIButton) {
+    }
 }
 
